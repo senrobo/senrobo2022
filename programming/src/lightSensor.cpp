074efcd2e30481302215fb93b0beb691490c4299
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-03-11 15:56:06 +0800

    edited stuff

--- programming/src/lightSensor.cpp
+++ programming/src/lightSensor.cpp
@@ -19,14 +19,18 @@ using namespace admux;
 /*
  * Creates a Mux instance.
  *
- * 1st argument is the SIG (signal) pin (Arduino analog input pin A0).
- * 2nd argument is the S0-S3 (channel control) pins (Arduino pins 8, 9, 10, 11).
+ * 1st argument is the SIG (signal) pin
+ * 2nd argument is the S0-S3 (channel control) pins
  */
-Mux mux(Pin(A0, INPUT, PinType::Analog), Pinset(8, 9, 10, 11));
+
+Mux muxOne(Pin(A0, INPUT, PinType::Analog), Pinset(8, 9, 10, 11));
+Mux muxTwo(Pin(A0, INPUT, PinType::Analog), Pinset(8, 9, 10, 11));
+Mux muxTwo(Pin(A0, INPUT, PinType::Analog), Pinset(8, 9, 10, 11));
 
 void setup() {
   // Serial port initialization.
-  Serial.begin(9600); while (!Serial) /* Waits for serial port to connect (needed for Leonardo only) */;
+  Serial.begin(115200); 
+  while (!Serial) /* Waits for serial port to connect (needed for Leonardo only) */;
 }
 
 /**
@@ -35,8 +39,8 @@ void setup() {
  */
 void loop() {
   int data;
-  for (byte i = 0; i < mux.channelCount(); i++) {
-    data = mux.read(i) /* Reads from channel i (returns a value from 0 to 1023) */;
+  for (byte i = 0; i < muxOne.channelCount(); i++) {
+    data = muxOne.read(i) /* Reads from channel i (returns a value from 0 to 1023) */;
 
     Serial.print("Potentiometer at channel "); Serial.print(i); Serial.print(" is at "); Serial.print((double) (data) * 100 / 1023); Serial.println("%%");
   }
